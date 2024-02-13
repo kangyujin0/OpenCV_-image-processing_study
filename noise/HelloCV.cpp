5d6e954cfa283a8644415daf3b1d74e30fb92b9e
wway555c@gmail.com <wway555c@gmail.com>
2024-02-13 16:48:17 +0900

    잡음 제거 필터링_가우시안 잡음 추가와 양방향 필터링

--- noise/HelloCV.cpp
+++ noise/HelloCV.cpp
@@ -5,9 +5,11 @@ using namespace cv;
 using namespace std;
 
 void noise_gaussian();
+void bilateral();
 int main()
 {    
-    noise_gaussian();
+    //noise_gaussian();
+    bilateral();
 }
 
 void noise_gaussian()
@@ -22,7 +24,7 @@ void noise_gaussian()
         randn(noise, 0, stddev); //평균이 0이고 표준 편차가 stddev인 가우시안 잡음 생성 noise행렬에 저장
 
         Mat dst;
-        add(src, noise, dst, Mat(), CV_8U);
+        add(src, noise, dst, Mat(), CV_8U); //Mat() -> mask
         
         //imshow("noise", noise);
         imshow("dst", dst);
@@ -33,4 +35,28 @@ void noise_gaussian()
     destroyAllWindows();
 
     return ;
+}
+
+void bilateral()
+{
+    Mat src = imread("C:/Users/EMBEDDED/Desktop/CV/images/lena.jpg", IMREAD_GRAYSCALE);
+    Mat noise(src.size(), CV_32SC1);
+    randn(noise, 0, 5); 
+
+    Mat dst;
+    add(src, noise, src, Mat(), CV_8U); //Mat() -> mask
+    imshow("src", src);
+
+    Mat dst1;
+    GaussianBlur(src, dst1, Size(), 5);
+    imshow("dst1", dst1);
+    
+    Mat dst2;
+    bilateralFilter(src, dst2, -1, 10, 30);
+    imshow("dst2", dst2);
+
+    waitKey();
+    destroyAllWindows();
+
+    return;
 }
